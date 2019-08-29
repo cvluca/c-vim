927375aea2a0c67304e53d248ea9267e3cf2d648
cvluca <z5155740@unsw.edu.au>
2019-08-30 02:20:05 +1000

    add vim-plug

--- install.sh
+++ install.sh
@@ -22,25 +22,32 @@ fail() {
 }
 
 install_vim () {
-  # backup old .vimrc file
+  # backup old .vimrc file and link
   if [[ -f "$HOME/.vimrc" ]]; then
-    mv "$HOME/.vimrc" "$HOME/.vimrc_old"
+    if [[ ! "$(readlink $HOME/.vimrc)" =~ $CURRENT_DIR/vimrc$ ]]; then
+      mv "$HOME/.vimrc" "$HOME/.vimrc_old"
+      ln -s "$CURRENT_DIR/vimrc" "$HOME/.vimrc"
+    fi
+  else
+    ln -s "$CURRENT_DIR/vimrc" "$HOME/.vimrc"
   fi
 
-  # backup .vim and install
+  # backup .vim folder and link
   if [[ -d "$HOME/.vim" ]]; then
-		if [[ ! "$(readlink $HOME/.vim)" =~ \.c-vim$ ]]; then
+		if [[ ! "$(readlink $HOME/.vim)" =~ $CURRENT_DIR$ ]]; then
 			mv "$HOME/.vim" "$HOME/.vim_old"
       ln -s "$CURRENT_DIR" "$HOME/.vim"
     fi
   else
     ln -s "$CURRENT_DIR" "$HOME/.vim"
   fi
-
   success "Installed c-vim for vim"
 }
 
 main () {
+  if [ ! -e "$CURRENT_DIR/vimrc" ]; then
+    fail "vimrc not exist"
+  fi
   install_vim
   exit 0
 }
---
+++ vimrc
@@ -0,0 +1,1 @@
+execute 'source' fnamemodify(expand('<sfile>'), ':h').'/.vim/config/main.vim'
