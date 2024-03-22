1e12e8dc57d974208559020bb2aa4d38dc1d276b
Luca Chen <Wei@cvluca.com>
2024-03-22 15:10:23 +0800

    update nvim options
    Signed-off-by: Luca Chen <Wei@cvluca.com>

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,24 @@
 require "nvchad.options"
 
--- add yours here!
+vim.opt.relativenumber = true
+
+vim.opt.swapfile = false
+
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = "→ ",
+  trail = "·",
+  precedes = "«",
+  extends = "»",
+}
+
+vim.cmd([[highlight! WhitespaceEOL ctermbg=grey guibg=grey]])
+vim.cmd([[match WhitespaceEOL /\s\+$/]])
+
+vim.cmd([[
+  augroup last_cursor_position
+    autocmd!
+    autocmd BufReadPost *
+      \ if line("'\"") > 1 && line("'\"") <= line("$") && &ft !~# 'commit' | execute "normal! g`\"zvzz" | endif
+  augroup END
+]])
