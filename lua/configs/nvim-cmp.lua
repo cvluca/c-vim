eac9c0490fe33d631682b2d5d974c7ac6183d4e0
Luca Chen <Wei@cvluca.com>
2024-04-12 00:05:27 +0800

    update cmp mapping
    Signed-off-by: Luca Chen <Wei@cvluca.com>

--- lua/configs/nvim-cmp.lua
+++ lua/configs/nvim-cmp.lua
@@ -1,3 +1,5 @@
+local cmp = require "cmp"
+
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
@@ -12,5 +14,40 @@ return {
       { name = "nvim_lua", group_index = 2 },
       { name = "path",     group_index = 2 },
     },
+    mapping = {
+      ["<C-p>"] = cmp.mapping.select_prev_item(),
+      ["<C-n>"] = cmp.mapping.select_next_item(),
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.close(),
+
+      ["<Tab>"] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = true,
+      },
+
+      ["<C-j>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif require("luasnip").expand_or_jumpable() then
+          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+
+      ["<C-k>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif require("luasnip").jumpable(-1) then
+          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+
+      ["<CR>"] = cmp.config.disable,
+    }
   },
 }
